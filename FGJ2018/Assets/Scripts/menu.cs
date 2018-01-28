cff60a95bd3d86c3e4bb90063627beec38e06723
KAMK FGJ2016 <FGJ2016@kamit.fi>
2018-01-28 15:39:05 +0200

    menu added

---
+++ FGJ2018/Assets/Scripts/menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    public KeyCode startKey;
+    public LobbyManager lobbyManager;
+    public Text startText;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+	void Update ()
+    {
+        if (Input.GetKey(startKey))
+        {
+            lobbyManager.showLobbyGUI = true;
+            lobbyManager.GetComponent<UnityEngine.Networking.NetworkManagerHUD>().showGUI = true;
+            startText.enabled = false;
+            
+        }
+		
+	}
+}
